a74fbfaa19bed14a1f7d33850cac2dbc464af37e
V <95189970+Vesec@users.noreply.github.com>
2023-02-10 12:16:08 -0800

    Add files via upload

---
+++ Scripts/calls.py
@@ -0,0 +1,64 @@
+ import os
+import psutil
+from gpiozero import CPUTemperature
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+def get_cpu_temperature():
+    #Return the CPU temperature as a string in Celsius.
+    cpu = CPUTemperature()
+    temp = cpu.temperature
+    return temp
+
+def get_ram_info():
+    #Return a list of information about RAM usage.
+    with os.popen("free") as p:
+        p.readline()
+        return list(map(int, p.readline().split()[1:4]))
+
+def get_cpu_use():
+    #Return the percentage of CPU used by the user as a percent value.
+    cpupercent=psutil.cpu_percent(4)
+    return cpupercent
+
+def get_disk_space():
+    #Return a list of information about disk space usage.
+    with os.popen("df -h /") as p:
+        p.readline()
+        return p.readline().split()[1:5]
+
+def get_ambient_temperature():
+    #Returns the temperature readings of the environment.
+    temperature = sense.get_temperature()
+    return temperature
+    
+def get_pressure():
+    #Returns the atmospheric pressure readings.
+    pressure = sense.get_pressure()
+    return pressure
+
+def get_humidity():
+    #Returns the humidity readings of the environment.
+    humidity = sense.get_humidity()
+    return humidity
+
+def get_gyro_data():
+    #Returns the angular velocity readings in the x, y, and z directions.
+    gyro_data = sense.get_gyroscope()
+    return gyro_data['x'], gyro_data['y'], gyro_data['z']
+
+def get_accelerometer_data():
+    #Returns the acceleration readings in the x, y, and z directions.
+    accelerometer_data = sense.get_accelerometer()
+    return accelerometer_data['roll'], accelerometer_data['pitch'], accelerometer_data['yaw']
+
+def get_orientation_radians():
+    #Returns the orientation readings in radians for pitch, roll, and yaw.
+    orientation_radians = sense.get_orientation_radians()
+    return orientation_radians['pitch'], orientation_radians['roll'], orientation_radians['yaw']
+
+def get_orientation_degrees():
+    #Returns the orientation readings in degrees for pitch, roll, and yaw.
+    orientation_degrees = sense.get_orientation_degrees()
+    return orientation_degrees['pitch'], orientation_degrees['roll'], orientation_degrees['yaw']
---
+++ Scripts/data.py
@@ -0,0 +1,38 @@
+import requests
+import time
+
+url = "https://pctips.ca/api/v1/s47B3i4t0oNlwFl67D5u/telemetry"
+headers = {'Content-Type': 'application/json'}
+
+while True:
+    # Generate some data to send
+    data = {}
+    data['temperature'] = calls.get_ambient_temperature()
+    data['cpu_temp'] = calls.get_cpu_temperature()
+    data['cpu_use'] = calls.get_cpu_use()
+    data['pressure'] = calls.get_pressure()
+    data['humidity'] = calls.get_humidity()
+    data['ram_total'] = calls.get_ram_info()[0]
+    data['ram_used'] = calls.get_ram_info()[1]
+    data['ram_free'] = calls.get_ram_info()[2]
+    data['disk_total'] = calls.get_disk_space()[0]
+    data['disk_used'] = calls.get_disk_space()[1]
+    data['disk_free'] = calls.get_disk_space()[2]
+    data['disk_used_percentage'] = calls.get_disk_space()[3]
+    data['acceleration'] = calls.get_accelerometer_data()
+    data['pitch'] = calls.get_orientation_degrees()[0]
+    data['roll'] = calls.get_orientation_degrees()[1]
+    data['yaw'] = calls.get_orientation_degrees()[2]
+
+
+    # Send the data to Thingsboard API
+    response = requests.post(url, json=data, headers=headers)
+
+    # Check for request success
+    if response.status_code == 200:
+        print("Data sent successfully")
+    else:
+        print("Failed to send data")
+
+    # Wait for 1 second before sending the next data
+    time.sleep(1)
